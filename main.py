158d984ebca99e0ad80dc1aa8c0682bf8254e04c
ericdelamata <2023_eric.de@iticbcn.cat>
2025-06-02 17:32:06 +0200

    exercici3

---
+++ main.py
@@ -0,0 +1,20 @@
+from services.users import get_user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.get("/users/user/", response_model = dict)
+async def get_user(name):
+    resp = get_user(name)
+    
+    return resp
---
+++ services/users.py
@@ -0,0 +1,31 @@
+import psycopg2
+from ..config.connect import connect
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_user(name):
+    conn = connect()
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''SELECT name, surname, email, description, course, year FROM "user"."user" WHERE name=%s;''', (name))
+        
+        user = cursor.fetchone()
+        
+        return user
+    except(Exception, psycopg2.Error) as error:
+        print("Error: ", error)
+        return {"Error": error}
