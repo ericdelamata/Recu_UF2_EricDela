90d0982422ba513822557bbc3e3b8bbad581b416
ericdelamata <2023_eric.de@iticbcn.cat>
2025-06-02 18:16:00 +0200

    exercici5

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from services.users import get_user, insert_user, update_user
 
 
-@app.post("users/update/", response_model=dict)
+@app.put("users/update/", response_model=dict)
 async def updateUser(name, surname, email, description, course, year, direction, CP, Password):
     correct = update_user(name, surname, email, description, course, year, direction, CP, Password)
     
