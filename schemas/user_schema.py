334fb6ad4569b1cda27032c6f086f6170ec5c4a3
ericdelamata <2023_eric.de@iticbcn.cat>
2025-06-02 18:01:41 +0200

    exerici4

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,4 +1,17 @@
 def user_schema(user) -> dict:
     resp = {
-        "name": user[0]
+        "name":user[0],
+        "surname":user[1],
+        "email":user[2],
+        "description":user[3],
+        "course":user[4],
+        "year":user[5],
     }
+    
+    return resp
+
+def users_schema(users) -> list[dict]:
+    
+    response = [user_schema(user) for user in users]
+    
+    return response
